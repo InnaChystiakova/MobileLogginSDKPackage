c22a72690a63e003bb300500c86264f8fe795785
InnaChystiakova <inna_islyamova@ukr.net>
2024-09-08 22:40:50 +0100

    Framework

---
+++ Frameworks/MobileLoggingSDK.xcframework/ios-arm64/MobileLogginSDK.framework/Headers/MobileLogginSDK.h
@@ -0,0 +1,16 @@
+//
+//  MobileLogginSDK.h
+//  MobileLogginSDK
+//
+//  Created by Inna Chystiakova on 06/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MobileLogginSDK.
+FOUNDATION_EXPORT double MobileLogginSDKVersionNumber;
+
+//! Project version string for MobileLogginSDK.
+FOUNDATION_EXPORT const unsigned char MobileLogginSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileLogginSDK/PublicHeader.h>
